663de1398daa3d6d7beebb0aac50acfc46c59311
d3da <daandieperink@gmail.com>
2023-12-21 12:08:02 +0100

    Add stuff to load_twitter.py to filter out irrelevant tweets

--- load_twitter.py
+++ load_twitter.py
@@ -1,27 +1,41 @@
-#!/usr/bin/env python3
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
 
 spark = SparkSession.builder.getOrCreate()
 
-path = '/data/twitterNL/201502/20150228-20.out.gz'
-
-df1 = spark.read.json(path)
-
-df1.printSchema()
-
-df2 = df1.select('id',
-                 'text',
-                 'user.lang',
-                 col('user.id').alias('user_id'),
-                 col('user.name').alias('username'),
-                 'user.verified',
-                 'timestamp_ms',
-                 'user.time_zone',
-                 'user.utc_offset')
-
-supported_langs = ['nl', 'en']
-df3 = df2.filter(col('lang').rlike('^' + '|'.join(supported_langs) + '$'))
+PATH = '/data/doina/Twitter-Archive.org/2017-01/01/*/*.json.bz2'
 
 
+def get_relevant_tweets_for_day(sparksession, path):
+    """
+    """
+    all_tweets = spark.read.json(path) \
+            .select('id',
+                    'text',
+                    'lang',
+                    col('user.lang').alias('user_lang'),
+                    col('user.id').alias('user_id'),
+                    col('user.name').alias('username'),
+                    'user.verified',
+                    'timestamp_ms',
+                    'user.time_zone',
+                    'user.utc_offset')
+
+    # Filter for only the langs that we support
+    supported_langs = ['nl', 'en']
+    tweets = all_tweets.filter(col('lang').rlike('^' + '|'.join(supported_langs) + '$'))
+
+    # Filter for only users that tweeted multiple times during the day (>5)
+    user_tweet_counts = tweets.groupBy('user_id').count().filter(col('count') > 5)
+
+    # TODO: Filter out the tweets that are not spread out enough through the day
+
+    # Only take the tweets from users who tweeted enough times
+    tweets_2 = tweets.join(user_tweet_counts, on='user_id')
+    return tweets_2, user_tweets_count
+
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.getOrCreate()
+    get_relevant_tweets_for_day(spark, PATH).show()
--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import filter
 import process
 import visualise
 
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
 #source files to use
-PATHS = ""
+#PATHS = ""
 
 #load the paths
-df_loaded = load_twitter.main(PATHS)
+df_loaded = load_twitter.get_relevant_tweets_for_day(spark, load_twitter.PATH)
 
 #filter users we need
 df_filtered = filter.main(df_loaded)
