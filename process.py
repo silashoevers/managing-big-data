567eaa0964e2443165cc292208381db6da2edee4
Kiona <k.bijker@student.utwente.nl>
2024-01-10 15:17:55 +0100

    Added the adding of new mistakes to the dataframe and updating the count

--- process.py
+++ process.py
@@ -16,8 +16,11 @@
 #   2) Calculates spelling mistakes as % of words in the tweet
 
 from textblob import TextBlob
+from pyspark.sql.functions import col,when
 
-def spell_check_word(language_code, word, df_mistakes_known):
+COLUMNS = ['lang_id','word','dist_count']
+
+def spell_check_word(language_code, word, df_mistakes_known,sparksession):
     #check if the word is in the known mistakes
     df_word = df_mistakes_known.filter(df_mistakes_known.lang_id == 'nl').filter(df_mistakes_known.word == word)
     dist = 0
@@ -30,17 +33,19 @@ def spell_check_word(language_code, word, df_mistakes_known):
         #TODO pick distance measure
         if dist>0:
         #if distance > 0, add the new spelling to the df with value and count
+            df_new_mistake = sparksession.createDataFrame([(language_code,word,(dist,1))],COLUMNS)
+            df_mistakes_known = df_mistakes_known.union(df_new_mistake)
 
     else:
         #if it is increase the associated count
         dist = df_word.first()['dist_count'][0]
         count = df_word.first()['dist_count'][1] + 1
-        #TODO figure out how to update a single value in a dataframe
-
+        df_mistakes_known = df_mistakes_known.withColumn('dist_count',when(col("word")==word,(dist,count)).otherwise(col('dist_count')))
+        
     #return distance and known mistakes df
     return dist, df_mistakes_known
 
-def spell_check_tweet(language_code, text, df_mistakes_known):
+def spell_check_tweet(language_code, text, df_mistakes_known,sparksession):
     #TODO map word spell checker over tweet text, then return total count
     return total_dist,df_mistakes_known
     
@@ -54,8 +59,7 @@ def main(sparksession,df_filtered_tweets):
     # spelling - this specific spelling of the word
     # distance - number of mistakes (based on [] distance between this spelling and closest known word)
     # count - number of times this mistake has been encountered, minimum of 1
-    columns = ['lang_id','word','dist_count']
-    df_mistakes_known = sparksession.createDataFrame([],columns)
+    df_mistakes_known = sparksession.createDataFrame([],COLUMNS)
     # TODO map spell checker for tweets over all tweets
     
     return df_filtered_tweets, df_mistakes_known
