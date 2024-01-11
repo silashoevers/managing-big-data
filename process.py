fae689873a05bc5b33900353f888e74a16b7bfcb
Kiona <k.bijker@student.utwente.nl>
2024-01-11 11:57:46 +0100

    Finished spell check word, still needs testing TODO: download and add dutch word list from open taal

--- process.py
+++ process.py
@@ -15,9 +15,9 @@
 #   1) Checks number of spelling mistakes in each tweet
 #   2) Calculates spelling mistakes as % of words in the tweet
 
-from textblob import TextBlob
 from pyspark.sql.functions import col,when
-from operator import add
+import nltk
+from nltk.metrics.distance import edit_distance
 
 # Make a df with encountered mistakes by language to make spell checking faster over time
 # This also keeps track of how often a specific mispelling is used
@@ -26,31 +26,50 @@ from operator import add
     # spelling   - String    - this specific spelling of the word
     # distance   - int       - number of mistakes (based on [] distance between this spelling and closest known word)
     # count      - int       - number of times this mistake has been encountered, minimum of 1
+    # word       - String    - registered spelling of the misspelled word (also named actual)
     # dist_count - (int,int) - contains a tuple of distance and count
-COLUMNS = ['lang_id','word','dist_count']
+COLUMNS = ['lang_id','spelling','dist_count_word']
 df_mistakes_known = sparksession.createDataFrame([],COLUMNS)
+
+# Correct word spelling lists
+nltk.download('words')
+from nltk.corpus import words #English words
+# Dutch word list source: https://github.com/OpenTaal/opentaal-wordlist
+#TODO add dutch word list
+#TODO check language tags accuracy
+rdd_correct_words = sparksession.sparkContext.paralellize(
+    [
+        ('en',words.words()),
+        ('nl',[])
+    ]
+)
 
 def spell_check_word(language_code, word,sparksession):
     #check if the word is in the known mistakes
-    df_word = df_mistakes_known.filter(df_mistakes_known.lang_id == 'nl').filter(df_mistakes_known.word == word)
+    df_word = df_mistakes_known.filter(df_mistakes_known.lang_id == language_code).filter(df_mistakes_known.spelling == word)
     dist = 0
     if df_word.isEmpty():
         #if it is not 
         #TODO figure out how to find the closest word (binary search by letter? since NLTK has bigrams)
-        #find the closest word
-
-        #determine the distance
-        #TODO pick distance measure
+        # source: https://www.geeksforgeeks.org/correcting-words-using-nltk-in-python/ 
+        #find the closest word and determine the edit distance
+        correct_words = rdd_correct_words.lookup(language_code)
+        possible_words = [(edit_distance(word, w),w) for w in correct_words if w[0]==word[0]] 
+        (dist,actual) = sorted(possible_words,key=lambda t:t[0])[0]
+        
+        #TODO set max distance of word to classify as different word
+        #TODO possible extension: determine count threshold to say 'accepted new spelling'
         if dist>0:
         #if distance > 0, add the new spelling to the df with value and count
-            df_new_mistake = sparksession.createDataFrame([(language_code,word,(dist,1))],COLUMNS)
+            df_new_mistake = sparksession.createDataFrame([(language_code,word,(dist,1,actual))],COLUMNS)
             df_mistakes_known = df_mistakes_known.union(df_new_mistake)
 
     else:
         #if it is increase the associated count
-        dist = df_word.first()['dist_count'][0]
-        count = df_word.first()['dist_count'][1] + 1
-        df_mistakes_known = df_mistakes_known.withColumn('dist_count',when(col("word")==word,(dist,count)).otherwise(col('dist_count')))
+        dist = df_word.first()['dist_count_word'][0]
+        count = df_word.first()['dist_count_word'][1] + 1
+        actual = df_word.first()['dist_count_word'][2]
+        df_mistakes_known = df_mistakes_known.withColumn('dist_count_word',when(col("spelling")==word,(dist,count,actual)).otherwise(col('dist_count_word')))
         
     #return distance
     return dist
