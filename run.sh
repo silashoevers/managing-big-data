c9ef7b8dee242b0ef6431b308520e8f74592766b
d3da <daandieperink@gmail.com>
2024-01-25 13:13:49 +0100

    Add spark application name and set maxExecutors=10

--- run.sh
+++ run.sh
@@ -9,10 +9,11 @@ set -x
 
 export PYSPARK_PYTHON=./venv/bin/python3
 spark-submit \
+    --name "Twitter spelling mistake analysis" \
     --master yarn \
     --archives pyspark_venv.tar.gz#venv \
     --py-files process.py,filter_tweets.py,visualise_analyse.py \
-    --conf spark.dynamicAllocation.maxExecutors=5 \
+    --conf spark.dynamicAllocation.maxExecutors=10 \
     main.py
 
 
